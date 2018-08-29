505ebd9acd939d6e74d7088f02e472586362fc23
Ihor Andriiv <andriivihor@icloud.com>
2018-08-29 09:20:52 +0300

    Update main.js

--- task2/public/main.js
+++ task2/public/main.js
@@ -5,65 +5,65 @@ searchButton.addEventListener('click', (e) => {
     const username = document.getElementById('username-input').value
     const age = document.getElementById('age-input').value
     axios.get(`http://localhost:3000/employees?first_name=${username}&age=${age}`)
-        .then(json=>{
+        .then(json => {
             console.log(json)
-           createDomElement(json['data'][0]);
+            createDomElement(json['data'][0]);
         })
-        .catch(err=>{
+        .catch(err => {
             errorFunction();
             console.log(err);
         })
 });
 
-function createDomElement(data){
+function createDomElement(data) {
     createTableHead(data);
     createTableBodyElems(data);
 }
 
-function  createTableBodyElems(data){
-    
-    const table=document.querySelector('table'),
-          tbody=document.createElement('tbody'),
-          tr=document.createElement('tr');
-    
-    for(let key in data){
-        let td=document.createElement('td');
-        td.textContent=data[key];
-        td.setAttribute("scope",'col');
-        tr.appendChild(td); 
+function createTableBodyElems(data) {
+
+    const table = document.querySelector('table'),
+        tbody = document.createElement('tbody'),
+        tr = document.createElement('tr');
+
+    for (let key in data) {
+        let td = document.createElement('td');
+        td.textContent = data[key];
+        td.setAttribute("scope", 'col');
+        tr.appendChild(td);
     }
-    tbody.appendChild(tr); 
+    tbody.appendChild(tr);
     table.appendChild(tbody);
 }
 
-function createTableHead(data){
-   
-    if (document.querySelector("table")){
+function createTableHead(data) {
+
+    if (document.querySelector("table")) {
         return;
     }
-    const main=document.querySelector('main'),
-        table=document.createElement('table'),
-        thead=document.createElement('thead'),
-        tr=document.createElement('tr');
+    const main = document.querySelector('main'),
+        table = document.createElement('table'),
+        thead = document.createElement('thead'),
+        tr = document.createElement('tr');
 
-        for (let key in data) {
-            let td=document.createElement('td');
-            td.textContent=key;
-            td.setAttribute("scope",'col');
-            tr.appendChild(td);
-        }
+    for (let key in data) {
+        let td = document.createElement('td');
+        td.textContent = key;
+        td.setAttribute("scope", 'col');
+        tr.appendChild(td);
+    }
 
     thead.classList.add('thead-dark');
     table.classList.add('table');
     table.classList.add('m-0');
-    table.style="margin: 0 auto";
+    table.style = "margin: 0 auto";
     thead.appendChild(tr);
-    table.appendChild(thead);          
+    table.appendChild(thead);
     main.appendChild(table);
 }
 
-function errorFunction(){
-    const h1=document.createElement('h1');
-    h1.textContent="We haven't got any data from api(((";
+function errorFunction() {
+    const h1 = document.createElement('h1');
+    h1.textContent = "We haven't got any data from api(((";
     document.body.appendChild(h1);
 }
